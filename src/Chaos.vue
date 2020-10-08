1ab9e92412fa19c73abb3baf58a698533cf59765
aviadhahami <aviad.hahami@snyk.io>
2020-10-08 14:36:40 +0300

    moving to ts

---
+++ src/Chaos.vue
@@ -0,0 +1,21 @@
+<template>
+<p>hi</p>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+const componentName = 'VueChaos';
+
+export default Vue.component(componentName, {
+  props:{
+    chance:{
+      default: false,
+      type: Boolean
+    },
+    errorMessage:{
+      default: `${componentName} created chaos!`,
+      type: String
+    }
+  }
+});
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Chaos from "@/Chaos.vue";
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(Chaos),
 }).$mount('#app')
