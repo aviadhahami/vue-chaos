c7a157aa183b2012e8c0cf6a9ff07318ec48481f
aviadhahami <aviad.hahami@snyk.io>
2020-10-10 23:18:56 +0300

    fixing main in pjson

--- demo/src/Demo.vue
+++ demo/src/Demo.vue
@@ -4,7 +4,7 @@
     {{instances}}
     <ErrorBoundary v-for="instance in instances" :key="instance" style="border:1px solid black;">
       odds of {{instance}}
-      <VueChaos :chance="5" error-message="MY error">
+      <VueChaos :chance="5" error-message="My error">
         I can emit chaos
       </VueChaos>
     </ErrorBoundary>
@@ -12,8 +12,8 @@
 </template>
 
 <script>
-import {VueChaos} from 'vue-chaos';
-import ErrorBoundary from 'vue-error-boundary';
+import * as VueChaos from 'vue-chaos';
+import * as ErrorBoundary from 'vue-error-boundary';
 export default {
   name: "Demo",
   components:{
@@ -22,7 +22,7 @@ export default {
   },
   data() {
     return {
-      a:"1'",
+      a:'1',
       instances: [...Array(Math.round(Math.random() * 7))].map((v, i) => i)
     }
   }
